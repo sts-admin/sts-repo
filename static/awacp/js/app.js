807fee9c579c4391f6d48dc045111797a522cca8
sts-admin <onlydeb143@gmail.com>
2016-12-23 23:47:48 +0530

    added manufactures link

--- static/awacp/js/app.js
+++ static/awacp/js/app.js
@@ -1,13 +1,14 @@
 
 (function() {
+    'use strict';
 	//Local env
 	var base ="http://localhost:8080/awacpservices";
 	var resourceReadPath = "http://localhost/awacp/resource/img/";
 	var basePath = "/awacp/";
-    'use strict';
 	//prod env
 	/*var base ="http://awacptechnicalservices.com:8080/awacpservices";
-	var resourceReadPath = "http://awacptechnicalservices.com/resource/img/";	*/
+	var resourceReadPath = "http://awacptechnicalservices.com/resource/img/";	
+	var basePath = "/";*/
     angular.module('awacpApp', ['awacpApp.services', 'awacpApp.controllers','angular-storage','ui.router','checklist-model', 'angularMoment', 'ui.bootstrap', 'angularjs-dropdown-multiselect', 'ui.navbar', 'ui.bootstrap.tpls', 'ds.clock','ui.select', 'ngSanitize','ui-listView','ngFileUpload', 'angucomplete-alt'])
 		.constant("base", base).constant("resourceReadPath", resourceReadPath).constant("basePath", basePath)
 		.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
@@ -237,6 +238,12 @@
 				templateUrl:"templates/qnotes.html",
 				controller:"QuoteNotesCtrl",
 				controllerAs:"qnoteVm",
+				requireAuth: true
+			}).state('manufactures',{
+				url: '/manufactures',
+				templateUrl:"templates/manufactures.html",
+				controller:"ManufactureCtrl",
+				controllerAs:"manuVm",
 				requireAuth: true
 			});
 			// if none of the above states are matched, use this as the fallback
---
+++ static/awacp/js/controllers/ManufactureCtrl.js
@@ -0,0 +1,35 @@
+(function() {
+	'use strict';
+	angular.module('awacpApp.controllers').controller('ManufactureCtrl', ManufactureCtrl);
+	ManufactureCtrl.$inject = ['$scope', '$state', '$location', '$http', 'AjaxUtil', 'store', '$q', '$timeout', '$window', '$rootScope', '$interval', '$compile', 'AlertService', '$uibModal', 'StoreService'];
+	function ManufactureCtrl($scope, $state, $location, $http, AjaxUtil, store, $q, $timeout, $window, $rootScope, $interval, $compile, AlertService, $uibModal, StoreService){
+		var manuVm = this;
+	    manuVm.totalItems = -1;
+		manuVm.currentPage = 1;
+		manuVm.pageNumber = 1;
+		manuVm.pageSize = 5;
+		$scope.timers = [];
+		manuVm.manufacturers= [];
+		manuVm.manufacture = {};
+		manuVm.addManufacture = function (title){		
+		}		
+		manuVm.pageChanged = function() {
+		};		
+		manuVm.cancelManufacture = function(){
+		}		
+		manuVm.editManufacture = function(){
+			if($state.params.id != undefined){				
+			}
+		}		
+		manuVm.getManufactures = function(){
+			manuVm.manufacturers = [];			
+		}
+		$scope.$on("$destroy", function(){
+			for(var i = 0; i < $scope.timers.length; i++){
+				$timeout.cancel($scope.timers[i]);
+			}
+		});
+	}		
+})();
+
+
